43dbf37d731532615e0a15d282a36495fd94c046
mittelholcz <dev.mittelholcz@gmail.com>
2019-04-23 16:29:26 +0200

    add textract

--- demo.py
+++ demo.py
@@ -4,6 +4,17 @@ from glob import glob
 from pathlib import Path
 import os.path
 import tika
+import textract
+
+
+def get_fout(fname, module):
+    """return the name of output file
+    """
+    res = os.path.basename(fname)
+    res = os.path.splitext(res)[0] + '.txt'
+    res = os.path.join('txt', module, res)
+    return res
+
 
 
 # TODO:
@@ -11,9 +22,13 @@ import tika
 #     pass
 
 
-# TODO:
-# def demo_textract(files):
-#     pass
+def demo_textract(files):
+    Path('txt/textract/').mkdir(parents=True, exist_ok=True)
+    for finp in files:
+        text = textract.process(finp, encoding='latin2')
+        text = text.decode(encoding='latin2')
+        with open(get_fout(finp, 'textract'), 'w') as f:
+            print(text, file=f)
 
 
 def demo_tika(files):
@@ -21,17 +36,15 @@ def demo_tika(files):
     tika.initVM()
     from tika import parser
     for finp in files:
-        fout = os.path.basename(finp)
-        fout = os.path.splitext(fout)[0] + '.txt'
-        fout = os.path.join('txt', 'tika', fout)
         parsed = parser.from_file(finp)
-        with open(fout, 'w') as f:
+        with open(get_fout(finp, 'tika'), 'w') as f:
             print(parsed["content"], file=f)
 
 
 def main():
     files = glob('pdf/*.pdf')
     demo_tika(files)
+    demo_textract(files)
 
 
 if __name__ == '__main__':
